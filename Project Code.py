2e27b870364ab6e00e1eae4885cd2c9ab2206ead
Ayesha_Noor_8874 <ayeshanoor8874@gmail.com>
2026-05-15 10:47:04 +0500

    GAME LOGIC Handles Board State, move validation and win/drw detection

--- Project Code.py
+++ Project Code.py
@@ -31,3 +31,99 @@ def drop_piece(board, col, player):
         return -1
     board[row][col] = player
     return row
+
+def undo_move(board, col):
+    for row in range(ROWS):
+        if board[row][col] != EMPTY:
+            board[row][col] = EMPTY
+            return row
+    return -1
+
+def check_winner(board, player):
+    #Horizontal
+    for row in range(ROWS):
+        for col in range(COLS - 3):
+            if all(board[row][col + i] == player for i in range(4)):
+                return True
+    
+    #Vertical
+    for col in range(COLS):
+        for row in range(ROWS - 3):
+            if all(board[row + i][col] == player for i in range(4)):
+                return True
+    
+    # Diagonal/
+    for row in range(ROWS - 3):
+        for col in range(COLS - 3):
+            if all(board[row + i][col + i] == player for i in range(4)):
+                return True
+    
+    #Diagnol\
+    for row in range(3, ROWS):
+        for col in range(COLS - 3):
+            if all(board[row - i][col + i] == player for i in range(4)):
+                return True
+            
+    return False
+
+def get_winning_cells(board, player):
+    #Horizontal 
+    for row in range(ROWS):
+        for col in range(COLS - 3):
+            cells = [(row, col + i) for i in range(4)]
+            if all(board[r][c] == player for r, c in cells):
+                return cells
+            
+    #Vertical
+    for col in range(COLS):
+        for row in range(ROWS - 3):
+            cells=[(row + i, col) for i in range(4)]
+            if all(board[r][c] == player for r, c in cells):
+                return cells
+            
+    #Diagnol /
+    for row in range(3, ROWS):
+        for col in range(COLS - 3):
+            cells = [(row + i, col + i) for i in range(4)]
+            if all(board[r][c] == player for r, c in cells):
+                return cells
+            
+    #Diagnol \
+    for row in range(3, ROWS):
+        for col in range(COLS - 3):
+            cells = ([row - i, col + i] for i in range(4))
+            if all(board[r][c] == player for r, c in cells):
+                return cells
+    
+    return []
+
+def is_terminal(board):
+    return (
+        check_winner(board, HUMAN)
+        or check_winner(board, AI)
+        or len(get_valid_columns(board)) == 0
+    )
+
+def print_board(board,winning_cells=None):
+    winning_cells = winning_cells or []
+    print()
+    print(" "+" ".join(str(c) for c in range(COLS)))
+    print(" "+"-----" * COLS)
+    symbols = {EMPTY: ".", HUMAN: "0", AI: "X"}
+
+    for row in range(ROWS):
+        row_str = "| "
+        for col in range(COLS):
+            cell =board[row][col]
+            sym = symbols[cell]
+            if (row,col) in winning_cells:
+                sym = "*"
+            row_str += sym + " |" if col < COLS - 1 else sym
+
+        print(row_str)
+
+    print(" " + "-----" * COLS)
+    print()
+
+
+
